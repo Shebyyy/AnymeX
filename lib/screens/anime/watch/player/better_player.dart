d06c1809ef68d68d643dcc6fc7f4e5d91bd632b0
Ryan _ <rehank220358@gmail.com>
2026-02-16 22:55:41 +0530

    fix: fixed a issue of resize mode not applying on player initialization

--- lib/screens/anime/watch/player/better_player.dart
+++ lib/screens/anime/watch/player/better_player.dart
@@ -370,6 +370,7 @@ class BetterPlayerImpl extends BasePlayer {
     double? width,
     double? height,
   }) {
+    _controller.setOverriddenFit(fit ?? BoxFit.contain);
     return BetterPlayer(
       controller: _controller,
     );
