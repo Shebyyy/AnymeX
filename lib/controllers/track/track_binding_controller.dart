27b6dcd5cd0e4edd7e0e51a2cc8dac5785e8e4a4
Shebyyy <83452219+Shebyyy@users.noreply.github.com>
2026-09-23 09:41:10 +0530

    fix(track): import ServiceHandler in track_binding_controller

--- lib/controllers/track/track_binding_controller.dart
+++ lib/controllers/track/track_binding_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:anymex/controllers/service_handler/params.dart';
+import 'package:anymex/controllers/service_handler/service_handler.dart';
 import 'package:anymex/controllers/services/anilist/anilist_data.dart';
 import 'package:anymex/controllers/services/mal/mal_service.dart';
 import 'package:anymex/controllers/services/simkl/simkl_service.dart';
